56fc683c37f5ce15c7fec226f923d455543af72a
vivekbhat0120 <vivekbhat0120@gmail.com>
2026-09-13 16:52:02 +0530

    rename to portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => ({
-  base: "/modern-portfolio/",
+  base: "/portfolio/",
   assetsInclude: ["**/*.glb"],
   server: {
     host: "::",
